f7143258130b9fbb53eb8b9949126fd54a864aac
JOHN PERRY CARPIO <isystem20@gmail.com>
2020-01-24 21:12:43 +0800

    Business Purpose - This will clasify the products or services of the particular business account which will help identify automatically the tax types linked to each account.

---
+++ aspnet-core/src/Hiraya.Domain/MongoDBCollections/Entities/BusinessPurpose.cs
@@ -0,0 +1,20 @@
+﻿using Hiraya.Domain.MongoDBCollections.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiraya.Domain.MongoDBCollections.Entities
+{
+    /// <summary>
+    /// This will clasify the products or services of the particular business account which will help identify automatically the tax types linked to each account.
+    /// </summary>
+    public class BusinessPurpose : AuditedEntity
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}

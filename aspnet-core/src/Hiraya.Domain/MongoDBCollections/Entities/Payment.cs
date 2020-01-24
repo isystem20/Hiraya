84b0e7748b1be14da908e943ee05397b6ebf5959
JOHN PERRY CARPIO <isystem20@gmail.com>
2020-01-24 23:11:22 +0800

    Payment details linked to the transaction items. This contains the important information related to the payment transactions to BIR.

---
+++ aspnet-core/src/Hiraya.Domain/MongoDBCollections/Entities/Payment.cs
@@ -0,0 +1,43 @@
+﻿using Hiraya.Domain.MongoDBCollections.Common;
+using Hiraya.Domain.MongoDBCollections.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiraya.Domain.MongoDBCollections.Entities
+{
+    /// <summary>
+    /// Payment details linked to the transaction items. This contains the important information related to the payment transactions to BIR.
+    /// </summary>
+    public class Payment : AuditedEntity
+    {
+        public string FormCode { get; set; } 
+        public string TaxpayerId { get; set; }
+        public string TransactionId { get; set; } //Transaction item should have the timestamp, etc
+        public YearType YearType { get; set; }
+        public string YearEnded { get; set; }
+        public YearQuarter Quarter { get; set; }
+        public DateTime DueDate { get; set; }
+        public string NoOfSheetsAttached { get; set; }
+        public string ATC { get; set; } // Im not sure what this is.
+        public DateTime ReturnPeriod { get; set; }
+        public string TaxCode { get; set; }
+        public string BCSNo { get; set; }
+        public string TaxpayerIdentificationNumber { get; set; }
+        public string RDOCode { get; set; }
+        public string TaxpayerClassification { get; set; }
+        public string LineOfBusinessOrOccupation { get; set; }
+        // Though Taxpayer Id is already included here, for counter checking and for history tracking, I still included the taxpayer details.
+        public string TaxpayerName { get; set; }
+        public string RegisteredAddress { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string ZipCode { get; set; }
+        public MannerOfPayment MannerOfPayment { get; set; }
+        public TypeOfPayment TypeOfPayment { get; set; }
+        public int NumberOfInstallment { get; set; }
+
+        
+    }
+}
---
+++ aspnet-core/src/Hiraya.Domain/MongoDBCollections/Enumerations/TypeOfPayment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiraya.Domain.MongoDBCollections.Enumerations
+{
+    public enum TypeOfPayment
+    {
+        Installment = 1,
+        PartialPayment = 2,
+        FullPayment = 3
+    }
+}

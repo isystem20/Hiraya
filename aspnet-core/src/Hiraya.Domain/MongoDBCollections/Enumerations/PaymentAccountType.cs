920d4200ded9f8242c562dea1b000f2d5809e1ef
JOHN PERRY CARPIO <isystem20@gmail.com>
2020-01-24 21:49:26 +0800

    PaymentAccountType - This will clasify the type of Payment Account.

---
+++ aspnet-core/src/Hiraya.Domain/MongoDBCollections/Enumerations/PaymentAccountType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiraya.Domain.MongoDBCollections.Enumerations
+{
+    /// <summary>
+    /// This will clasify the type of Payment Account.
+    /// </summary>
+    public enum PaymentAccountType
+    {
+        BankAccount = 1,
+        PaymentService = 2,
+        Card = 3
+    }
+}

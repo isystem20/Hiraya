9671c5c620b766b2a3153d7cf78801137b9f18e7
JOHN PERRY CARPIO <isystem20@gmail.com>
2020-01-24 21:16:54 +0800

    This will clasify the type of reminder.

---
+++ aspnet-core/src/Hiraya.Domain/MongoDBCollections/Enumerations/ReminderType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiraya.Domain.MongoDBCollections.Enumerations
+{
+    public enum ReminderType
+    {
+        Private = 0,
+        Common = 1,
+        Custom = 2,
+        Appointment = 3,
+        Important = 4
+    }
+}

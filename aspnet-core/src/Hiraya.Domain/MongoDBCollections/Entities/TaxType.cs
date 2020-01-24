fd212ec000897590791318f12ed8a9085041a601
JOHN PERRY CARPIO <isystem20@gmail.com>
2020-01-24 22:05:45 +0800

    TaxType - This will be the Tax Type for the tax payments.

---
+++ aspnet-core/src/Hiraya.Domain/MongoDBCollections/Entities/TaxType.cs
@@ -0,0 +1,19 @@
+﻿using Hiraya.Domain.MongoDBCollections.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiraya.Domain.MongoDBCollections.Entities
+{
+    /// <summary>
+    /// This will be the Tax Type for the tax payments.
+    /// </summary>
+    public class TaxType : AuditedEntity
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}

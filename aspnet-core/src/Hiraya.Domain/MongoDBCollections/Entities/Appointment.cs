1fb6b6b26b6750b403dc2ef47a249cee368e509b
Raymart030493 <raymart04entuna@gmail.com>
2020-01-29 20:10:39 +0800

    ***Add Appointment

---
+++ aspnet-core/src/Hiraya.Domain/MongoDBCollections/Entities/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Domain.Entities.Auditing;
+using Hiraya.Domain.MongoDBCollections.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hiraya.Domain.MongoDBCollections.Entities
+{
+    public class Appointment : AuditedEntity
+    {
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string BussinesPurpose { get; set; }
+        public string Notes { get; set; }
+        public DateTime StartDate { get; set; } //Startdate of appointment
+        public DateTime EndDate { get; set; } //Startdate of appointment
+        public Status Status { get; set; } //Finished
+        
+    }
+}

c855e795568e9fbf08300ece63bdce04c222a646
Raymart030493 <raymart04entuna@gmail.com>
2020-01-29 20:11:14 +0800

    ***Add Transaction Details, Not Yet Done I need help about the details.

---
+++ aspnet-core/src/Hiraya.Domain/MongoDBCollections/Entities/TransactionDetail.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hiraya.Domain.MongoDBCollections.Entities
+{
+    public class TransactionDetail : AuditedEntity
+    {
+        public string TransactionId { get; set; }
+
+        //nde ku pa mapicture out yung ibang details
+    }
+}

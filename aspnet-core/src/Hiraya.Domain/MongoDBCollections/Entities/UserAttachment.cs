2d1c828dbef7de49c61605e3718b7225580156a3
Raymart030493 <raymart04entuna@gmail.com>
2020-01-29 20:11:36 +0800

    ***Add User Attachments

---
+++ aspnet-core/src/Hiraya.Domain/MongoDBCollections/Entities/UserAttachment.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hiraya.Domain.MongoDBCollections.Entities
+{
+    public class UserAttachment : AuditedEntity
+    {
+        public string Attachments { get; set; }
+        public int UserId { get; set; }
+        public string Remarks { get; set; }
+    }
+}
